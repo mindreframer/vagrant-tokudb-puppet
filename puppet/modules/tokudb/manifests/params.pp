54e3244681edd1e42b67d5c1b6733a9be5f564b4
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-30 15:03:37 +0200

    actual download url

--- puppet/modules/tokudb/manifests/params.pp
+++ puppet/modules/tokudb/manifests/params.pp
@@ -2,4 +2,6 @@ class tokudb::params{
   $fullpath      = "mysql-5.5.28-tokudb-6.6.4-52174-linux-x86_64"
   $download_file = "$fullpath.tar.gz"
   $packagenames  = ["libaio1", "mysql-client-core-5.5"] # ubuntu 12.04
+  # a tmp location
+  $download_url  = "http://master.dl.sourceforge.net/project/tokudbdownload/mysql-5.5.28-tokudb-6.6.4-52174-linux-x86_64.tar.gz"
 }
